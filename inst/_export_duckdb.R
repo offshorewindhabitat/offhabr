d8d3cf12fc098b1d6035d9242fedd17af2c2a01e
Ben Best <ben@ecoquants.com>
2023-03-07 18:50:59 -0800

    drop iris test table

--- inst/_export_duckdb.R
+++ inst/_export_duckdb.R
@@ -1,9 +1,16 @@
 librarian::shelf(
-  devtools, glue, here)
+  DBI, devtools, glue, here)
 load_all()
 
 con <- oh_con()
 
+# make edits
+# con <- oh_con(read_only = F)
+# dbRemoveTable(con, "iris")
+# dbDisconnect(con, shutdown=T)
+
 dir_export <- here("inst/_export_duckdb")
 sql <- glue("EXPORT DATABASE '{dir_export}'")
 dbExecute(con, sql)
+
+dbDisconnect(con, shutdown=T)
